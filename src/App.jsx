309d9b2c2e2f3b0da94f85f736a29c2763692a15
Tommy Pedersen <tommp@hiof.no>
2025-03-17 07:27:34 +0100

    yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { resources } from "./assets/ressurser";
 import { DOMContentLoaded } from "./assets/ressursbytte";
-import "./styles/style.css";
+import "./style/style.css";
 
 function App() {
   return (
